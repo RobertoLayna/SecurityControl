41f9ae20e9e6319df389a9d90caf0617426c9d68
RobertoLayna <potter.laynar@gmail.com>
2022-05-04 14:08:22 -0500

    password change resident

--- src/components/resident/Home.vue
+++ src/components/resident/Home.vue
@@ -14,8 +14,74 @@
             {{ residential.residential_name }}
           </div>
         </v-card-text>
+        <v-card-actions>
+          <v-btn
+            primary
+            outlined
+            block
+            @click="dialog = true"
+          >
+            Cambiar contraseña
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-col>
+    <v-dialog
+      v-model="dialog"
+      transition="dialog-bottom-transition"
+      max-width="600"
+    >
+      <v-card>
+        <v-toolbar
+          color="primary"
+          dark
+        >
+          Cambiar contraseña
+        </v-toolbar>
+        <v-card-text>
+          <v-row class="pt-3">
+            <v-col
+              cols="12"
+            >
+              <v-text-field
+                v-model="olderPass"
+                label="Contraseña actual"
+                outlined
+                clearable
+                hide-details
+              />
+            </v-col>
+          </v-row><v-row class="pt-3">
+            <v-col
+              cols="12"
+            >
+              <v-text-field
+                v-model="newPass"
+                label="Nueva contraseña"
+                outlined
+                clearable
+                hide-details
+              />
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions class="justify-end">
+          <v-btn
+            color="error"
+            outlined
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="success"
+            @click="updatePassword()"
+          >
+            Aceptar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-row>
 </template>
 
@@ -24,8 +90,11 @@ import { mapGetters } from 'vuex'
 export default {
   data() {
     return {
+      dialog: false,
       residence: {},
-      residential: {}
+      residential: {},
+      olderPass: null,
+      newPass: null,
     }
   },
   
@@ -68,6 +137,41 @@ export default {
         })
         .catch((error) => {
           return []
+        })
+    },
+    async updatePassword() {
+      this.userUpdate = await this.$axios
+        .post(
+          'https://53ea886.online-server.cloud/app/updatePassword', {
+            id: this.user.user_id,
+            olderPass: this.olderPass,
+            newPass: this.newPass
+          }
+        )
+        .then((rs) => {
+          this.dialog = false
+          
+      this.$store.dispatch('logout').then(()=>{
+        this.$router.push({name: "Login"})
+      })
+    
+        })
+        .catch((error) => {
+          console.log('PreLogin', err)
+        this.$toast.error(err, {
+            position: 'bottom-center',
+            timeout: 5000,
+            closeOnClick: true,
+            pauseOnFocusLoss: true,
+            pauseOnHover: true,
+            draggable: true,
+            draggablePercent: 0.6,
+            showCloseButtonOnHover: false,
+            hideProgressBar: true,
+            closeButton: 'button',
+            icon: true,
+            rtl: false
+          })
         })
     }
   }
